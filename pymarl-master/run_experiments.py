83b3a7e85bda9c580fa5d95fdcc99c6c841912e3
joosep <joosephook@gmail.com>
2021-03-25 22:06:31 +0000

    Add missing utils module

--- pymarl-master/run_experiments.py
+++ pymarl-master/run_experiments.py
@@ -1,9 +1,9 @@
 #envs="WrappedCheckers WrappedCombat WrappedLumberjacks WrappedPongDuel WrappedPredatorPrey WrappedSwitch WrappedTrafficJunction"
-# envs="WrappedCheckers WrappedPredatorPrey"
-envs="WrappedPredatorPrey"
+envs="WrappedCheckers WrappedPredatorPrey"
+# envs="WrappedPredatorPrey"
 import subprocess
 
-for i in range(2**31-2, 2**31):
+for i in range(2**31-3, 2**31):
   for env in envs.split():
     cmdstr = f'python src/main.py --config=qplex --env-config={env} with local_results_path="./{env}/results/" use_tensorboard=True seed={i}'
     print(cmdstr)
